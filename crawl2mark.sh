76815d2b7e7b80567330e1b9abe2d52ef8ff44f4
Raffael Zica <raffael@pixeltastic.de>
2020-03-17 21:39:42 +0100

    Improved shell runner and css style

--- crawl2mark.sh
+++ crawl2mark.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+ORIGIN="$PWD"
+
+cd "$DIR" || exit 1
 
 if [[ ! -d "node_modules" ]]; then
-    yarn
+    npm install
 fi
 
 URL=$1
 echo "Converting $URL to markdown..."
 TEMPDIR=$(mktemp -d)
-CSD=$(dirname "$(readlink -f "$0")")
-mkdir -p "$CSD/render"
-mkdir -p "$CSD/output"
+mkdir -p "$DIR/render"
+mkdir -p "$DIR/output"
 echo "Render page..."
-OUTHTML=$(ts-node "$CSD/src/app.ts" "$URL")
+OUTHTML=$(ts-node "$DIR/src/app.ts" "$URL")
 if [[ $? -gt 0 ]]; then
     echo "Browser error!"
     exit 1
 fi
 echo "Improve html output..."
-yarn prettier --write "$OUTHTML" 2>/dev/null
+npx prettier --write "$OUTHTML" 2>/dev/null
 DOCNAME=$(basename "$OUTHTML")
 DOCNAME="${DOCNAME%.*}.md"
 echo "Generating $DOCNAME..."
-pandoc -r html-native_divs-native_spans -t gfm+pipe_tables+fenced_code_blocks+gfm_auto_identifiers+backtick_code_blocks+autolink_bare_uris+space_in_atx_header+intraword_underscores+strikeout+emoji+shortcut_reference_links+angle_brackets_escapable+lists_without_preceding_blankline --wrap=none --strip-comments --atx-headers -s "$OUTHTML" -o "$CSD/output/$DOCNAME"
+pandoc -r html-native_divs-native_spans -t gfm+pipe_tables+fenced_code_blocks+gfm_auto_identifiers+backtick_code_blocks+autolink_bare_uris+space_in_atx_header+intraword_underscores+strikeout+emoji+shortcut_reference_links+angle_brackets_escapable+lists_without_preceding_blankline --wrap=none --strip-comments --atx-headers -s "$OUTHTML" -o "$ORIGIN/$DOCNAME"
+
+cd "$ORIGIN" || exit 1
